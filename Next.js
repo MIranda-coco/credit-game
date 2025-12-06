4ee87b0c49befc05647e338c6084b0fb92eb1f8a
MIranda-coco <cocomirandalee@gmail.com>
2025-12-06 16:40:50 +0800

    Create Next.js

---
+++ Next.js
@@ -0,0 +1,7 @@
+# 创建项目
+npx create-next-app@latest bounty-hunter --typescript --tailwind --app
+cd bounty-hunter
+
+# 安装依赖
+npm install mongoose bcryptjs jsonwebtoken next-auth
+npm install @types/bcryptjs @types/jsonwebtoken -D
